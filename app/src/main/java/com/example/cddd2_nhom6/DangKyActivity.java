c64d80158864105ca6b89965fca66a1c1b927aed
huynh-toan <hqtoan07@gmail.com>
2024-10-11 16:04:12 +0700

    Giao dien man hinh dang nhap dang ki

--- app/src/main/java/com/example/cddd2_nhom6/DangKyActivity.java
+++ app/src/main/java/com/example/cddd2_nhom6/DangKyActivity.java
@@ -8,7 +8,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.xemphim.R;
 
 public class DangKyActivity extends AppCompatActivity {
 
@@ -17,10 +16,6 @@ public class DangKyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dang_ky);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
